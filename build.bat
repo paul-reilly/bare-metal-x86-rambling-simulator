f0e0ba7f337dc6ef5e8e2e47490a8076987f4671
Paul Reilly <paul-reilly@users.noreply.github.com>
2020-12-30 22:09:32 +0000

    creates bin dir if not exists

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
+@rem prepare
+    @echo off
+    echo Checking that dir 'bin' exists...
+    if not exist "bin" mkdir "bin"
+
 
 @rem assemble our files
-    @echo off
+    echo Assembling files...
     cd src
     @echo on
     nasm boot_sector.asm -f bin -o ../bin/boot_sector.bin
@@ -8,10 +13,9 @@
     nasm boot_pm.asm -f bin -o ../bin/image.bin
     @echo off
     cd ..
-    @echo on
 
 @rem moosh them together into one binary
-    @echo off
+    echo Mooshing binary objects together...
     cd bin
     @echo on
     cat boot_sector.bin adventure.bin > image.bin
