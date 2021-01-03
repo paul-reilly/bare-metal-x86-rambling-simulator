afe25afe15b9cab0c674ab00b5851fb65933637f
Paul Reilly <paul-reilly@users.noreply.github.com>
2021-01-03 13:43:30 +0000

    uses si register for print_string / formatting

--- src/print_functions_16.asm
+++ src/print_functions_16.asm
@@ -7,61 +7,62 @@ SCROLL_TELETYPE equ 0x0e
 ; input: al - char to print
 ; output-
 writechar:
-    ;push ax
-    mov ah, SCROLL_TELETYPE
-    int 0x10
-    ;pop ax
-    ret
+  mov ah, SCROLL_TELETYPE
+  int 0x10
+  ret
 
 
-;    MSG: db 'ros loading... please wait, this should only take a moment', 0
-; input: bx - address of null-terminated string
-; output-
 print_string:
-    push ax
-    mov ah, SCROLL_TELETYPE
+; inp: si - address of null-terminated string
+; out:
+  push bx
+  push ax
+  mov ah, SCROLL_TELETYPE
   .loopstart:
-    cmp [bx], byte 0
+    lodsb                         ; loads [si] into al and increments si
+    cmp al, 0
     je .finish
-    mov al, [bx]
+    mov bh, 0x00                  ; display page 0
+    mov bl, 0x07                  ; text attribute
     int 0x10
-    inc bx
     jmp .loopstart
   .finish:
-    pop ax
-    ret
+  pop ax
+  pop bx
+  ret
 
 
 ; input: al - contains byte
 ; output-
 print_byte:
-    push ax
-    cmp al, 10
-    jge .letter
-    add al, 48        ; ascii numbers start at 48 
+  push ax
+  cmp al, 10
+  jge .is_letter
+  .is_number:
+    add al, 48                    ; ascii numbers start at 48 
     jmp .cont
-  .letter:
-    add al, 55        ; 65 is ascii 'A' and 10 = 'A'
+  .is_letter:
+    add al, 55                    ; 65 is ascii 'A' and 10 = 'A'
   .cont:
-    call writechar
+  call writechar
 
-    pop ax
-    ret
+  pop ax
+  ret
 
 
 ;    prints value in dx as hex
 ; input: dx - our 2 bytes
 ; output-
 print_hex:
-    pusha
-    mov al, '0'
-    call writechar
-    mov al, 'x'
-    call writechar
-    ; we have four nibbles in our 16 bits to print
-    mov cl, 12                  ; first bit shift value
+  pusha
+  mov al, '0'
+  call writechar
+  mov al, 'x'
+  call writechar
+  ; we have four nibbles in our 16 bits to print
+  mov cl, 12                      ; first bit shift value
   .loopstart:
-    mov bx, dx                  ; preserve dx
+    mov bx, dx                    ; preserve dx
     shr bx, cl
     and bx, 0x000f
     mov al, bl
@@ -72,5 +73,5 @@ print_hex:
     
     jmp .loopstart
   .finish:
-    popa
-    ret
+  popa
+  ret
